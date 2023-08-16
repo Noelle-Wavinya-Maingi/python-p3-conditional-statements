353130650208c0807ec5830d798cc4d08901485f
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-16 09:35:57 +0300

    hows_the_weather() completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,6 +10,14 @@ def admin_login(username, password):
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature >= 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
